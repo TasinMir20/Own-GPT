d192b5e7c74edc1aba867e2ffdf6ac2c571e31f2
Tasin Mir <tasinmir20@gmail.com>
2023-08-05 00:30:30 +0600

    Backend api update

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
 	// messageDiv.innerHTML = "..."
 	loader(messageDiv);
 
-	const response = await fetch("https://tasin-pamzzr88z-tasinmir20.vercel.app/", {
+	const response = await fetch("https://tasin.vercel.app/", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
